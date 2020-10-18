0d7558d40500195f848f3bcad4302206d83d9139
Gurev Aleksey <gurevaleksey@yandex.ru>
2020-10-18 18:55:30 +0300

    Выполнил ДЗ 14

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,18 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+let description = 'list';
+let flag = false;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>
+        <h1 style = {{
+          color: 'green'
+        }}>
           To Do
         </h1>
+        <p>
+          {/* вывод переменной */}
+          {description}
+        </p>
+        <p>
+          {/* вывод числа */}
+          {24}
+        </p>
+        <p>
+          {/* вывод арифметической операции */}
+          {24+50}
+        </p>
+        <p>
+          {/* логическая операция */}
+          {description && 'logic'  }
+        </p>
+        <p>
+          {/* тернарная операция */}
+          {flag ? 'true' : 'false'}
+        </p>
+        <p>
+          {/* вывод undefined, null, true, false */}
+          {undefined}
+          {true}
+          {false}
+          {null}
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
