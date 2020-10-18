b496683f038ee3b44429087e0b21c1ebcc5e3e38
Gurev Aleksey <gurevaleksey@yandex.ru>
2020-10-18 22:31:59 +0300

    Выполнил задание 15

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as nums from './number';
 let description = 'list';
 let flag = false;
 
@@ -9,46 +10,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 style = {{
-          color: 'green'
-        }}>
-          To Do
-        </h1>
         <p>
-          {/* вывод переменной */}
-          {description}
+          {nums.count * nums.length}
         </p>
-        <p>
-          {/* вывод числа */}
-          {24}
-        </p>
-        <p>
-          {/* вывод арифметической операции */}
-          {24+50}
-        </p>
-        <p>
-          {/* логическая операция */}
-          {description && 'logic'  }
-        </p>
-        <p>
-          {/* тернарная операция */}
-          {flag ? 'true' : 'false'}
-        </p>
-        <p>
-          {/* вывод undefined, null, true, false */}
-          {undefined}
-          {true}
-          {false}
-          {null}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
