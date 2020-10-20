3a3a55fb2168bbe80db95b083f6ab2ccdd21633e
Gurev Aleksey <gurevaleksey@yandex.ru>
2020-10-20 15:27:08 +0300

    Добавил css modules

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import InputItems from '../InputItems/InputItems';
 import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
-import '../App/App.css';
+import styles from '../App/App.module.css';
 import '../Fonts/Fonts.css';
 
 let TodoItems = [
@@ -22,8 +22,8 @@ let Numbers = 3;
 
 
 let App = () => (
-  <section className = 'container'>
-    <h1 className = 'main-title'>To DO:</h1>
+  <section className = {styles.container}>
+    <h1 className = {styles.title}>To DO:</h1>
     <InputItems />
       <ItemList TodoItems = { TodoItems }/>
     <Footer count = { Numbers } />
