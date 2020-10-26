5169e2eb970f26a1404b0e34864ee2db26c85f5f
Gurev Aleksey <gurevaleksey@yandex.ru>
2020-10-26 09:14:41 +0300

    добавил кнопку "добавить задачу"

--- src/components/InputItems/InputItems.js
+++ src/components/InputItems/InputItems.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import styles from '../InputItems/InputItems.module.css';
 import TextField from '@material-ui/core/TextField';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
 
 let InputItems = () => (
-  <TextField 
-    id="standard-basic" 
-    label="Добавить задачу"
-    fullWidth
-    variant="outlined"
-  />
+  <div>
+    <div className={styles.container}>
+      <TextField 
+        id="standard-basic" 
+        label="Добавить задачу"
+        fullWidth
+        variant="outlined"
+      />
+    </div>
+    <div className={styles.containerFab}>
+      <Fab
+        style={{ marginTop: 30}} 
+        size="small" 
+        color="primary" 
+        aria-label="add">
+        <AddIcon />
+      </Fab>
+    </div>
+  </div>
 );
 
 export default InputItems;
