79f153a4793b654e99b5b2f31accc0aed8cee5f7
Gurev Aleksey <gurevaleksey@yandex.ru>
2020-10-26 11:44:02 +0300

    добавил удаление элемента

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -3,7 +3,7 @@ import Item from '../Item/Item';
 import styles from './ItemList.module.css'
 import classnames from 'classnames';
 
-let ItemList = ({ TodoItems, onClickDone }) => (<ul className = {
+let ItemList = ({ TodoItems, onClickDone, onClickRemove }) => (<ul className = {
   classnames ({
     [styles.list]: true
   })
@@ -14,7 +14,8 @@ let ItemList = ({ TodoItems, onClickDone }) => (<ul className = {
       item = {item.value} 
       isDone = {item.isDone}
       id = {item.id} 
-      onClickDone = {onClickDone} 
+      onClickDone = {onClickDone}
+      onClickRemove = {onClickRemove} 
       />
     </li> )}
   </ul>
