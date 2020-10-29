df082bcac99668d053bde80838939295b205786a
Gurev Aleksey <gurevaleksey@yandex.ru>
2020-10-29 10:27:52 +0300

    добавил вывод жизненного цикла

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -6,31 +6,49 @@ import Switch from '@material-ui/core/Switch';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 
-let Item = ({ item, isDone, id, onClickDone, onClickRemove }) => (
-<div className = {styles.container}>
-  <Switch
-    // checked={state.checkedB}
-    // onChange={handleChange}
-    color="primary"
-    name="checkedB"
-    onClick = {() => onClickDone(id)}
-  />
 
-  <p className={
-    classnames({
-      [styles.item]: true,
-      [styles.done]: isDone
-    })
-    }>{ item }
-  </p>
-  
-  <IconButton style={{ padding: 7}} aria-label="delete"
-    onClick = {() => onClickRemove(id)} 
-  >
-    <DeleteIcon/>
-  </IconButton>  
-</div>
-);
+class Item extends React.Component {
+
+  componentDidMount() {
+    console.log('componentDidMount')
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate')
+  }
+
+  componentWillUnmount () {
+    console.log('componentWillUnmount')
+  }
+
+  render() {
+   let { item, isDone, id, onClickDone, onClickRemove } = this.props;
+
+    return (
+      <div className = {styles.container}>
+        <Switch
+          color="primary"
+          name="checkedB"
+          onClick = {() => onClickDone(id)}
+        />
+
+        <p className={
+          classnames({
+            [styles.item]: true,
+            [styles.done]: isDone
+          })
+          }>{ item }
+        </p>
+        
+        <IconButton style={{ padding: 7}} aria-label="delete"
+          onClick = {() => onClickRemove(id)} 
+        >
+          <DeleteIcon/>
+        </IconButton>  
+      </div>
+    );
+  }
+}
 
 Item.propTypes = {
   id: PropTypes.number.isRequired,
